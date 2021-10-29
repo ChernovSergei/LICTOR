7fc61e90a8fe0991a78ae15bca479e10bfceb275
ChernovSergei <chernov.v.sergey@yandex.ru>
2021-10-29 16:10:35 +0800

    Second commit. Header and its IDs modification

--- maintenance/Resource.h
+++ maintenance/Resource.h
@@ -1,30 +1,44 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by maintenance.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_MAINTENANCE_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_MAINTENANCE			107
-#define IDI_SMALL				108
-#define IDC_MAINTENANCE			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// Next default values for new objects
 //
+#define IDC_MYICON                      2
+#define IDD_MAINTENANCE_DIALOG          102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_MAINTENANCE                 107
+#define IDI_SMALL                       108
+#define IDC_MAINTENANCE                 109
+#define IDR_MAINFRAME                   128
+#define ID_FILE_NEWWO                   1001
+#define ID_DATABASE_FIELDSITES          1002
+#define ID_DATABASE_EQUIPMENT           1003
+#define ID_DATABASE_COMPANIES           1004
+#define ID_DATABASE_UNITS               1005
+#define ID_DATABASE_UNIT                1006
+#define ID_DATABASE_FSRS                1007
+#define ID_DATABASE_HOLYDAYS            1008
+#define ID_FILE_SAVE                    1009
+#define ID_REPORT_FIELDSITEREPORT       1010
+#define ID_REPORT_SALARYREPORT          1011
+#define ID_REPORT_BORESCOPEREPORT       1012
+#define ID_REPORT_VIBRA                 1013
+#define ID_REPORT_SALARYSUMMARYREPORT   1014
+#define ID_REPORT_WOSSUMMARY            1015
+#define ID_FILE_SETTINGS                1016
+#define ID_HELP_README                  1017
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32788
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
